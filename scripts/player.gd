0736249460a2720bb235f3adfa09b48a534f7092
Arjen van Dijk <arjen_commit@proton.me>
2026-08-16 17:38:40 +0200

    Implemented basic inventory system

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 75.0
+@export var speed : float = 75.0
 
+@export var inventory: Inventory
 
 func _physics_process(delta: float) -> void:
 	## Get Input direction on a 2d vector
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	
 	## Calculate velocity
-	velocity = input_direction * SPEED
+	velocity = input_direction * speed
 		
 	## Debug
 	print("x: " + str(round(position.x)) + " y: " + str(round(position.y)))
 
 	move_and_slide()
+
+func collect(item):
+	inventory.insert(item)
