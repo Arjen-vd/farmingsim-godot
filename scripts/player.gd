8e2a2d8c23350b1f2918046d60d41dfef03d400c
Arjen van Dijk <arjen_commit@proton.me>
2026-08-16 14:17:38 +0200

    Shortened player movement script and normalized movement speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,18 +5,13 @@ const SPEED = 75.0
 
 
 func _physics_process(delta: float) -> void:
-	var direction_x := Input.get_axis("Left", "Right")
-	var direction_y := Input.get_axis("Up", "Down")
-	if direction_x:
-		velocity.x = direction_x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	## Get Input direction on a 2d vector
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	
+	## Calculate velocity
+	velocity = input_direction * SPEED
 		
-	if direction_y:
-		velocity.y = direction_y * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-	print("x: " + str(velocity.x) + " y: " + str(velocity.y))
+	## Debug
+	print("x: " + str(round(position.x)) + " y: " + str(round(position.y)))
 
 	move_and_slide()
