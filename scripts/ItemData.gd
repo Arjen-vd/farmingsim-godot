e20dfff28b385e6e1576ab23cd11a0efc20502ae
Arjen van Dijk <arjen_commit@proton.me>
2026-08-20 17:37:03 +0200

    Added audio for pickup

--- scripts/ItemData.gd
+++ scripts/ItemData.gd
@@ -2,7 +2,19 @@ extends Resource
 
 class_name ItemData
 
+enum Type {ITEM, FOOD, TOOL, CLOTHING}
+
+enum Rarity {COMMON, UNCOMMON, RARE, EPIC, LEGENDARY}
+
+
 @export var name: String
 @export_multiline var description : String
-@export var maxStack: int
-@export var texture: Texture2D
+
+@export_category("Item Stats")
+@export var basePrice : int
+@export var maxStack : int
+@export var type : Type
+@export var rarity : Rarity
+
+@export_category("Display")
+@export var texture : Texture2D
